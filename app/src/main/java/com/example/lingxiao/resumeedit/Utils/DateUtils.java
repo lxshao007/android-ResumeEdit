3f0c960532bc27a6349aef2809f82951f26f5baa
Lingxiao Shao <lingxiaoahao@gmail.com>
2018-03-20 18:02:41 -0700

    added fake data to mock dynamic ui

---
+++ app/src/main/java/com/example/lingxiao/resumeedit/Utils/DateUtils.java
@@ -0,0 +1,8 @@
+package com.example.lingxiao.resumeedit.Utils;
+
+/**
+ * Created by lingxiao on 3/20/18.
+ */
+
+public class DateUtils {
+}
---
+++ app/src/main/java/com/example/lingxiao/resumeedit/model/Project.java
@@ -0,0 +1,8 @@
+package com.example.lingxiao.resumeedit.model;
+
+/**
+ * Created by lingxiao on 3/20/18.
+ */
+
+public class Project {
+}
